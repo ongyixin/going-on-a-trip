363e4fff1c49ec26e42bc1fec6d3fa6bd4158294
yyyam0693 <e1354172@u.nus.edu>
2025-01-19 00:46:05 +0800

    Delete my-chrome-extension/background.js

--- my-chrome-extension/background.js
+++
@@ -1,8 +0,0 @@
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//   console.log("message received", message);
-//   if (message.action === 'startTrip') {
-//       console.log(`Starting ${message.funLevel} trip `);
-//       sendResponse({message: "service worker processed the message"});
-//   }
-//   return true;
-// });
